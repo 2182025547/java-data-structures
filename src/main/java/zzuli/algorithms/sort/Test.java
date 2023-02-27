3998487887dc1402571561d27f248075cae7cab0
Crean culty <2082025547@qq.com>
2023-02-27 09:32:56 +0800

    　code

--- src/main/java/zzuli/algorithms/sort/Test.java
+++ src/main/java/zzuli/algorithms/sort/Test.java
@@ -4,21 +4,22 @@ package zzuli.algorithms.sort;
 
 public class Test {
     public static void main(String[] args) {
-//        int[] a={98,90,34,56,21};
+        int[] a={98,90,34,56,21};
 //        int[] b={5,2,4,6,1,3};
 //        int[] c={2,4,5,7,1,2,3,6};
+        QuickSort.quickSort(a,0,a.length-1);
         //bulleSort(a);
 //        SelectionSort.selectionSort(a);
 //        InsertionSort.insertionSort(b);
 //        ShellSort.shellSort(b);
 //        MergeSort.mergeSort(c,0,c.length-1);
-        int a2[] = new int[]{11,9,11,13,12,14,15,13};
-        int[] b2 = new int[a2.length];
-        CountingSort.countingSort(a2,b2,15);
-        System.out.println();
-        for(int num:b2){
-            System.out.print(num+" ");
-        }
+//        int a2[] = new int[]{11,9,11,13,12,14,15,13};
+//        int[] b2 = new int[a2.length];
+//        CountingSort.countingSort(a2,b2,15);
+//        System.out.println();
+//        for(int num:b2){
+//            System.out.print(num+" ");
+//        }
 
     }
 }
--- src/main/java/zzuli/learnjava/Collections/Collection_.java
+++ src/main/java/zzuli/learnjava/Collections/Collection_.java
@@ -6,19 +6,28 @@ public class Collection_ {
     //Map
     //Collection
     public static void main(String[] args) {
-        List arrayList = new ArrayList();
-        arrayList.add("jack");
-        arrayList.add("Tom");
-        ///
-        Iterator iterator = arrayList.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-
-        }
-        for (Object o : arrayList) {
-            System.out.println("o = " + o);
-        }
-
+//        List arrayList = new ArrayList();
+//        arrayList.add("jack");
+//        arrayList.add("Tom");
+//        ///
+//        Iterator iterator = arrayList.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.next());
+//
+//        }
+//        for (Object o : arrayList) {
+//            System.out.println("o = " + o);
+//        }
+        Collection<String> c = new ArrayList<>();
+        c.add("a");
+        c.add("a");
+        c.add("a");
+        c.add("a");
+        c.add("a");
+        c.add("a");
+        String[] sc = c.toArray(new String[0]);
+        Object[] sc2 = c.toArray();
+        System.out.println(sc.length);
 
     }
 
